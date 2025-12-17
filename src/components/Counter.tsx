bbd3caf2066d359d74488c8026e541e438f5a21d
Abenezer <abitysank@gmail.com>
2025-12-16 21:27:35 -0500

    Add state and basic controls to Counter component
    useState  implemented for handling a basic count with number type, and default value in the counter component. Basic increment, decrement and reset button implemented using its setter function. and basic step value element added

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,6 +1,9 @@
 import type React from "react";
+import { useState } from "react";
 
 export const Counter: React.FC = () => {
+    const [count, setCount] = useState<number>(0);
+
     return (
         <div>
             <div>
@@ -8,15 +11,27 @@ export const Counter: React.FC = () => {
                     <h1>Counter App</h1>
                 </div>
                 <div>
-                    <p>Current Count:</p>
+                    <p>Current Count: {count}</p>
                 </div>
                 <div>
                     <div>
-                        <button>Decrement</button>
-                        <button>Increment</button>
-                        <button>Reset</button>
+                        <button
+                            onClick={() => count > 0 && setCount(count - 1)}
+                        >
+                            Decrement
+                        </button>
+
+                        <button onClick={() => setCount(count + 1)}>
+                            Increment
+                        </button>
+                        <button onClick={() => setCount(0)}>Reset</button>
+                    </div>
+                    <div>
+                        <label htmlFor="value">Step Value</label>
+                        <input type="number" min={1} value={1} />
                     </div>
                 </div>
+
                 <div>
                     <p>Count History:</p>
                 </div>
